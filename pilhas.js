45c4f30387145b8642d1fc255e1ff32250faa047
eduardonk9999 <thiago.novak7@gmail.com>
2022-02-03 16:45:32 -0300

    Add, pilhas

---
+++ pilhas.js
@@ -0,0 +1,33 @@
+function Stack() {
+  let items = []
+
+  this.push = function(element) {
+    // add um novo item à pilha
+  }
+
+  this.pop = function() {
+    //remover o item do topo da pilha
+  }
+
+  this.peek = function() {
+    // devolve o elemento que está no topo da pilha
+  }
+
+  this.isEmpty = function() {
+    //informa se a pilha está vazia ou não
+  }
+
+  this.clear = function() {
+    // limpa a pilha
+  }
+
+  this.size = function() {
+    // informa o tamanho da pilha
+  }
+
+  this.print = function() {
+    //imprime a pilha no console
+  }
+}
+
+
