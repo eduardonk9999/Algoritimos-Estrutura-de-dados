b927dae7f5184c46c542f286e32d39546af3378f
eduardonk9999 <thiago.novak7@gmail.com>
2022-02-03 16:51:08 -0300

    Add, incluindo methods do calss Stack

--- PIlhas/pilhas.js
+++ PIlhas/pilhas.js
@@ -3,30 +3,37 @@ function Stack() {
 
   this.push = function(element) {
     // add um novo item à pilha
+    this.items.push(element)
   }
 
   this.pop = function() {
     //remover o item do topo da pilha
+    return this.items.pop()
   }
 
   this.peek = function() {
     // devolve o elemento que está no topo da pilha
+    return this.items[this.items.length - 1]
   }
 
   this.isEmpty = function() {
     //informa se a pilha está vazia ou não
+    return this.items.length === 0
   }
 
   this.clear = function() {
     // limpa a pilha
+    this.items = []
   }
-
+  
   this.size = function() {
     // informa o tamanho da pilha
+    return this.items.length
   }
 
   this.print = function() {
     //imprime a pilha no console
+
   }
 }
 
